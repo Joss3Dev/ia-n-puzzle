104484d283a50bc7a8c84146e1db6f23421400e0
Jose <jose.luis.veron@hotmail.com>
2021-08-12 01:03:35 -0400

    Se agrega funcion para mover y otra para verificar si es estado final

--- Estado.py
+++ Estado.py
@@ -1,10 +1,13 @@
+import math, time
+
+
 class Estado:
 
-    def __init__(self, matriz):
+    def __init__(self, matriz, pos_vacio, padre, altura):
         self.matriz = matriz
-        self.pos_vacio = 0
-        self.padre = 0
-        self.altura = 0
+        self.pos_vacio = pos_vacio
+        self.padre = padre
+        self.altura = altura
 
     def bfs(self, estado_inicial):
         """
@@ -18,11 +21,19 @@ class Estado:
         """
         cola = []
         cola.append(estado_inicial)
-        while len(cola) > 0:
+        nodos_expandidos = 0
+        final = None
+        t_ini = time.time()
+        while len(cola) > 0 or not final:
+            nodos_expandidos += 1
             estado_actual = cola.pop()
-            # VERIFICA SI ES QUE ES UN ESTADO FINAL O NO
-            estados_siguientes = estado_actual.evaluar_movimientos()
-            cola.append(estados_siguientes)
+            if estado_actual.es_estado_final():
+                final = estado_actual
+            else:
+                estados_siguientes = estado_actual.evaluar_movimientos()
+                cola.append(estados_siguientes)
+        t_fin = time.time()
+        t_total = t_fin - t_ini
 
     def evaluar_movimientos(self):
         """
@@ -41,5 +52,44 @@ class Estado:
         Si esta en la frontera superior, osea, 0 <= indice <= cantidad_columnas - 1, entonces no puede moverse hacia arriba
         Si esta en la frontera inferior, osea, N - cantidad_columnas + 1 <= indice <= N, entonces no puede moverse hacia abajo
         """
+        nuevos_estados = []
+        cant_cols = math.sqrt(len(self.matriz))
+        if not (self.pos_vacio + 1) % cant_cols == 0:
+            nuevo_estado = self.mover('der')
+            nuevos_estados.append(nuevo_estado)
+        if not self.pos_vacio % cant_cols == 0:
+            nuevo_estado = self.mover('izq')
+            nuevos_estados.append(nuevo_estado)
+        if not 0 <= self.pos_vacio <= cant_cols - 1:
+            nuevo_estado = self.mover('arr')
+            nuevos_estados.append(nuevo_estado)
+        if not (cant_cols ** 2 - cant_cols) <= self.pos_vacio < len(self.matriz):
+            nuevo_estado = self.mover('abj')
+            nuevos_estados.append(nuevo_estado)
 
+    def mover(self, dir):
+        matriz = self.matriz
+        indice = self.pos_vacio
+        if dir == 'der':
+            matriz[indice], matriz[indice+1] = matriz[indice+1], matriz[indice]
+            indice = indice + 1
+        if dir == 'izq':
+            matriz[indice], matriz[indice-1] = matriz[indice-1], matriz[indice]
+            indice = indice - 1
+        if dir == 'arr':
+            cant_cols = math.sqrt(len(self.matriz))
+            matriz[indice], matriz[indice - cant_cols] = matriz[indice - cant_cols], matriz[indice]
+            indice = indice - cant_cols
+        if dir == 'abj':
+            cant_cols = math.sqrt(len(self.matriz))
+            matriz[indice], matriz[indice + cant_cols] = matriz[indice + cant_cols], matriz[indice]
+            indice = indice + cant_cols
+        nuevo_estado = Estado(matriz, indice, self, self.altura + 1)
+        return nuevo_estado
 
+    def es_estado_final(self):
+        matriz = self.matriz
+        for i in range(len(matriz)-1):
+            if matriz[i] > matriz[i + 1]:
+                return False
+        return True
