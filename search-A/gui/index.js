ead1c14a00797a401dc29521e970550f0d978037
Jose <jose.luis.veron@hotmail.com>
2021-08-15 02:04:29 -0400

    Se rellena la tabla con los datos del puzzle generado

--- search-A/gui/index.js
+++ search-A/gui/index.js
@@ -4,9 +4,36 @@ async function generarMatriz() {
     if ( n ){
         let valores = await eel.generarEstadoInicial(n)();
         console.log(Array.from(valores));
-        
+
+        var table_body_nueva = document.createElement('tbody');
+        indice = 0
+
+        for (var i = 0; i < n; i++){
+            var tr = document.createElement('tr');
+            for (var j = 0; j < n; j++){
+                var td = document.createElement('td');
+                var text = document.createTextNode(valores[indice]);
+                td.appendChild(text);
+                tr.appendChild(td);
+                indice++;
+            }
+            table_body_nueva.appendChild(tr);
+        }
+        table_body_nueva.classList.add("text-center");
+        table_body_nueva.setAttribute("id", "tabla_inicial_body");
+        var tabla_body_original = document.getElementById("tabla_inicial_body");
+        var tabla = document.getElementById("tabla_inicial");
+        tabla.replaceChild(table_body_nueva, tabla_body_original)
     }
     else{
         alert("Ingrese un valor para N")
     }
+}
+
+async function buscarResolucion(){
+    let algoritmo = document.getElementById("algotritmo").value;
+    let n = document.getElementById("n").value;
+    // BUSCAR FORMA DE OBTENER EL ESTADO INICIAL Y VALIDAR QUE YA SE HAYA GENERADO
+    // SEPARAR PARA ELEGIR VARIOS ALGORITMOS
+
 }
